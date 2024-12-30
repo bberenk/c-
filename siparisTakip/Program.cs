598639508497613556fb3f8ca9ef1aad1d7cdccd
bberenk <120375808+bberenk@users.noreply.github.com>
2024-12-30 18:09:54 +0300

    Add files via upload

---
+++ siparisTakip/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace siparisTakip
+{
+    public delegate void SiparisIslemi(string mesaj);
+    internal class Program
+    {
+        //sipariş işlemleri için metotlar
+        public static void mutfagaBildirim(string mesaj)
+        {
+            Console.WriteLine("");
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine(mesaj);
+            Console.ResetColor();
+        }
+        public static void musteriyeBildirim(string mesaj)
+        {
+            Console.WriteLine("");
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine(mesaj);
+            Console.ResetColor();
+        }
+
+        public static void kasayaBildirim(string mesaj)
+        {
+            Console.WriteLine("");
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine(mesaj);
+            Console.ResetColor();
+        }
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Sipariş Takip Sistemi");
+            Console.WriteLine("1- Sipariş Oluştur");
+
+
+            //Tekil Delegate Kullanımı
+
+            Console.WriteLine("Tekil Delegate Kullanımı");
+            SiparisIslemi tekilDelegate = mutfagaBildirim;
+            tekilDelegate("Mutfakta sipariş oluşturuldu");
+
+            //Çoklu Delegate Kullanımı
+
+            Console.WriteLine("");
+            Console.WriteLine("Çoklu Delegate Kullanımı");
+            SiparisIslemi cokluDelegate = mutfagaBildirim;
+            cokluDelegate += musteriyeBildirim;
+            cokluDelegate += kasayaBildirim;
+            cokluDelegate("Sipariş oluşturuldu");
+
+            //Generic Delegate Kullanımı - Action
+
+            Console.WriteLine("");
+            Console.WriteLine("Generic Delegate Kullanımı");
+            Action<string> actionOrnek = tesekkurMesajiGonder;
+            tesekkurMesajiGonder("Teşekkürler, yine bekleriz");
+
+            //Generic Delegate Kullanımı - Func
+            Func<int, int, int> funcOrnek = siparisToplamHesapla;
+            Console.WriteLine("Toplam: " + funcOrnek(100, 18));
+
+            //Generic Delegate Kullanımı - Predicate
+            Predicate<string> siparisHazirmi = siparisHazir;
+            Console.WriteLine("Sipariş hazır mı: " + siparisHazirmi("Hazır"));
+
+            //Anonim Delegate Kullanımı
+
+            SiparisIslemi anonimDelegate = delegate (string mesaj)
+            {
+                Console.WriteLine("");
+                Console.BackgroundColor = ConsoleColor.Yellow;
+                Console.WriteLine(mesaj);
+                Console.ResetColor();
+            };
+
+            anonimDelegate("Sipariş teslim ediliyor..");
+        }
+
+        static void tesekkurMesajiGonder(string mesaj)
+        {
+            Console.WriteLine("");
+            Console.BackgroundColor = ConsoleColor.Magenta;
+            Console.WriteLine(mesaj);
+            Console.ResetColor();
+        }
+
+        static int siparisToplamHesapla(int tutar, int kdv)
+        {
+            return tutar + kdv;
+        }
+
+        static bool siparisHazir(string durum)
+        {
+            if (durum == "Hazır")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ siparisTakip/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("siparisTakip")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("siparisTakip")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("77b10fed-9b7a-4bae-a416-e7f703e8c3ce")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
